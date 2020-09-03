fefba3844e00717cdb6969638a5429416b0fb56f
tonylinkflow <tony@linkflow.co.kr>
2020-09-03 15:22:29 +0900

    added, vr library for preview

--- app/src/main/java/com/linkflow/fitt360sdk/activity/PreviewActivity.java
+++ app/src/main/java/com/linkflow/fitt360sdk/activity/PreviewActivity.java
@@ -1,9 +1,16 @@
 package com.linkflow.fitt360sdk.activity;
 
+import android.graphics.SurfaceTexture;
+import android.media.MediaCodec;
+import android.media.MediaFormat;
+import android.opengl.EGL14;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -15,20 +22,28 @@ import androidx.annotation.Nullable;
 import com.android.mediacodec.AVPacket;
 import com.android.mediacodec.Packet;
 import com.android.mediacodec.VideoDecoder;
+import com.asha.vrlib.MDVRLibrary;
+import com.asha.vrlib.texture.MD360VideoTexture;
 import com.linkflow.fitt360sdk.R;
 
+import java.nio.ByteBuffer;
+
+import app.library.linkflow.Constant;
 import app.library.linkflow.manager.NeckbandRestApiClient;
 import app.library.linkflow.manager.item.RecordSetItem;
 import app.library.linkflow.manager.model.StitchingModel;
 import app.library.linkflow.manager.neckband.NotifyManage;
 import app.library.linkflow.rtsp.AudioDecoderThread;
 import app.library.linkflow.rtsp.RTSPStreamManager;
+import app.library.linkflow.rtsp.VideoDecodeThread;
 
-public class PreviewActivity extends BaseActivity implements SurfaceHolder.Callback {
+public class PreviewActivity extends BaseActivity {
     private final String TAG = getClass().getSimpleName();
     private static final int MSG_NOT_START_RTSP = 10;
     private RTSPStreamManager mRTSPStreamManager;
 
+    private GLSurfaceView mGLSurfaceView;
+    private MDVRLibrary mVRLibrary;
     private Handler mRTSPChecker;
     private Button mMuteBtn, mStableBtn;
     private boolean mIsStable;
@@ -39,8 +54,7 @@ public class PreviewActivity extends BaseActivity implements SurfaceHolder.Callb
         super.onCreate(savedInstanceState);
         setBodyView(R.layout.activity_preview);
 
-        SurfaceView surfaceView = findViewById(R.id.surface);
-        surfaceView.getHolder().addCallback(this);
+        mGLSurfaceView = findViewById(R.id.surface);
 
         mMuteBtn = findViewById(R.id.audio);
         mMuteBtn.setOnClickListener(this);
@@ -63,6 +77,68 @@ public class PreviewActivity extends BaseActivity implements SurfaceHolder.Callb
                 }
             }
         };
+
+        initRenderer();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mVRLibrary.onResume(this);
+    }
+
+    private void initRenderer() {
+        mVRLibrary = MDVRLibrary.with(this)
+                .displayMode(MDVRLibrary.DISPLAY_MODE_NORMAL)
+                .interactiveMode(MDVRLibrary.INTERACTIVE_MODE_MOTION | MDVRLibrary.INTERACTIVE_MODE_MOTION_WITH_TOUCH)
+                .asVideo(new MDVRLibrary.IOnSurfaceReadyCallback() {
+                    @Override
+                    public void onSurfaceReady(Surface surface) {
+                        Log.e("preview", "on surface ready");
+                        mRTSPChecker.removeMessages(MSG_NOT_START_RTSP);
+                        mRTSPChecker.sendEmptyMessageDelayed(MSG_NOT_START_RTSP, 6000);
+                        RecordSetItem recordSetItem = mNeckbandManager.getSetManage().getRecordSetItem();
+                        mRTSPStreamManager = RTSPStreamManager.builder().setAsyncDataListener(new VideoDecoder.AsyncDataListener() {
+                            @Override
+                            public void asyncData(int type, Packet packet) {
+                                // packet has all information about stream. you can use it.
+                                if (type == AVPacket.PT_VIDEO) {
+
+                                } else if (type == AVPacket.PT_AUDIO) {
+
+                                }
+                            }
+
+                            @Override
+                            public boolean disablePreview() {
+                                return false;
+                            }
+                        }).setFrameCallback(new VideoDecoder.FrameCallback() {
+                            @Override
+                            public void hasFrame() {
+                                if (mRTSPChecker.hasMessages(MSG_NOT_START_RTSP)) {
+                                    mNeckbandManager.setPreviewState(true);
+                                    mRTSPChecker.removeMessages(MSG_NOT_START_RTSP);
+                                }
+                            }
+                        }).setAudioDecodedListener(new AudioDecoderThread.DecodedListener() {
+                            @Override
+                            public void decodedAudio(byte[] audioData, int offset, int length) {
+
+                            }
+                        }).setResolution(recordSetItem.getWidth(), recordSetItem.getHeight()).build();
+                        mRTSPStreamManager.setUrl(NeckbandRestApiClient.getRTSPUrl());
+                        mRTSPStreamManager.setSurface(surface);
+                        mRTSPStreamManager.start();
+                    }
+
+                }).listenGesture(new MDVRLibrary.IGestureListener() {
+                    @Override
+                    public void onClick(MotionEvent e) {
+
+                    }
+                })
+                .build(mGLSurfaceView);
     }
 
     @Override
@@ -88,6 +164,7 @@ public class PreviewActivity extends BaseActivity implements SurfaceHolder.Callb
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        mVRLibrary.onDestroy();
         if (mRTSPStreamManager != null) {
             mRTSPStreamManager.stop();
         }
@@ -110,56 +187,6 @@ public class PreviewActivity extends BaseActivity implements SurfaceHolder.Callb
                 }
             }
         });
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        Log.e(TAG, "surface created");
-        mRTSPChecker.removeMessages(MSG_NOT_START_RTSP);
-        mRTSPChecker.sendEmptyMessageDelayed(MSG_NOT_START_RTSP, 6000);
-        RecordSetItem recordSetItem = mNeckbandManager.getSetManage().getRecordSetItem();
-        mRTSPStreamManager = RTSPStreamManager.builder().setAsyncDataListener(new VideoDecoder.AsyncDataListener() {
-            @Override
-            public void asyncData(int type, Packet packet) {
-                // packet has all information about stream. you can use it.
-                if (type == AVPacket.PT_VIDEO) {
-
-                } else if (type == AVPacket.PT_AUDIO) {
-
-                }
-            }
-
-            @Override
-            public boolean disablePreview() {
-                return false;
-            }
-        }).setFrameCallback(new VideoDecoder.FrameCallback() {
-            @Override
-            public void hasFrame() {
-                if (mRTSPChecker.hasMessages(MSG_NOT_START_RTSP)) {
-                    mNeckbandManager.setPreviewState(true);
-                    mRTSPChecker.removeMessages(MSG_NOT_START_RTSP);
-                }
-            }
-        }).setAudioDecodedListener(new AudioDecoderThread.DecodedListener() {
-            @Override
-            public void decodedAudio(byte[] audioData, int offset, int length) {
-
-            }
-        }).setResolution(recordSetItem.getWidth(), recordSetItem.getHeight()).build();
-        mRTSPStreamManager.setUrl(NeckbandRestApiClient.getRTSPUrl());
-        mRTSPStreamManager.setSurface(holder.getSurface());
-        mRTSPStreamManager.start();
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        Log.e(TAG, "surface changed");
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        Log.d(TAG, "surface destroyed");
     }
 
     @Override
